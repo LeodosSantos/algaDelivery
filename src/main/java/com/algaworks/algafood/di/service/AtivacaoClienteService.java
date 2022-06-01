aa79dea5b747feea28f744ba96ed8736507850c7
lsantos <leonardo.santos@atendecerto.com.br>
2022-06-01 20:03:44 -0300

    Publicando e consumindo eventos customizados

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -4,7 +4,7 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.di.modelo.Cliente;
@@ -15,22 +15,11 @@ import com.algaworks.algafood.di.notificacao.TipoDoNotificador;
 //@Component
 public class AtivacaoClienteService {
 
-	@TipoDoNotificador(Value = NivelUrgencia.SEM_URGENCIA)
 	@Autowired
-	private Notificador notificador; /* o noftificador está sempre nulo */
-
-	//@PostConstruct
-	private void init() {
-		System.out.println("INIT" + notificador);
-	}
-
-	//@PreDestroy
-	private void destroy() {
-		System.out.println("DESTROY");
-	}
-
+	private ApplicationEventMulticaster eventMulticaster;
+	
 	public void ativar(Cliente cliente) {
 		cliente.ativar();
-		notificador.notificar(cliente, "Seu cadastro no sistema está ativo! ");
+		eventMulticaster.publishEvent(new ClienteAtivadoEvent(cliente));
 	}
 }
---
+++ src/main/java/com/algaworks/algafood/listener/NotificacaoService.java
@@ -0,0 +1,27 @@
+package com.algaworks.algafood.listener;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.di.notificacao.NivelUrgencia;
+import com.algaworks.algafood.di.notificacao.Notificador;
+import com.algaworks.algafood.di.service.ClienteAtivadoEvent;
+
+@Component
+public class NotificacaoService {
+	
+	@TipoNotificado(NivelUrgencia.URGENTE)
+	@Autowired
+	private Notificador notificador;
+	
+	@EventListener
+	public void clienteAtivadoListener(ClienteAtivadoEvent event) {
+		notificador.notificar(event.getCliente(), :"");
+		
+		System.out.println("cliente" + event.getCliente().getNome() + " agora está ativo");
+		
+		
+	}
+
+}
