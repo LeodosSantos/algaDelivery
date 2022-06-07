a6569850607716d94a7010909f526cdd4dbc5906
lsantos <leonardo.santos@atendecerto.com.br>
2022-06-07 18:44:35 -0300

    Consultando objetos do banco de dados

---
+++ src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
@@ -0,0 +1,25 @@
+package com.algaworks.algafood.jpa;
+
+import java.util.List;
+
+import javax.management.Query;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import org.springframework.stereotype.Component;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+
+@Component
+public class CadastroCozinha {
+
+	@PersistenceContext
+	EntityManager manager;
+
+	public List<Cozinha> listar() {
+		return manager.createQuery("from Cozinha", Cozinha.class).getResultList();
+
+	}
+
+}
