93a435f1a66f88a0fba7d8b215592c10f51c5fbe
lsantos <leonardo.santos@atendecerto.com.br>
2022-06-15 19:39:20 -0300

    Consultando Singleton Resource com GET e @PathVariable

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,18 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
-//@Controller-- ? -- pode apagar
-//@ResponseBody-- ? -- pode apaga
+//@Controller-- componente spring controlador -- pode apagar
+//@ResponseBody-- quando for acionado algum método get irá ter uma resposta -- pode apaga
 @RestController // @RestController contém o @controller e o @ReponseBody
-@RequestMapping("/cozinha")
+@RequestMapping(value = "/cozinha")
 public class CozinhaController {
-	
+
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
-	
-	@GetMapping
-	public List<Cozinha> listar(){
+
+	@GetMapping // requisições com ret http serão encaminhada para esse método
+	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
-		
 	}
+
+	@GetMapping("/{cozinhaId}") //  concatenando --->  /cozinhas/{cozinhaId}
+	public Cozinha buscar(@PathVariable Long cozinhaId) {
+		return cozinhaRepository.buscar(cozinhaId);
+	}
+
 }
