318d8fff17eaefa5326252c6dd14d2a9fc5e2e93
lsantos <leonardo.santos@atendecerto.com.br>
2022-06-27 19:27:00 -0300

    Desafio: Modelando e implementando a atualização de recursos de restaurantes

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -2,15 +2,16 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
@@ -24,37 +25,56 @@ public class RestauranteController {
 
 	@Autowired
 	private RestauranteRepository restauranteRepository;
-	
+
 	@Autowired
 	private CadastroRestauranteService CadastroRestauranteService;
-	
+
 	@GetMapping
 	public List<Restaurante> listar() {
 		return restauranteRepository.listar();
 	}
 
 	@GetMapping("/{restauranteId}")
-	public ResponseEntity<Restaurante> buscar(@PathVariable Long restauranteId){
-		
+	public ResponseEntity<Restaurante> buscar(@PathVariable Long restauranteId) {
+
 		Restaurante restaurante = restauranteRepository.buscar(restauranteId);
-				
+
 		if (restaurante != null) {
 			return ResponseEntity.ok(restaurante);
 		}
 		return ResponseEntity.notFound().build();
 	}
-	
-	@PostMapping // foi substuituido o <Restaurante> por <?> devido conflito pois só estava não aceitaria a string
-	public ResponseEntity<?>	 adicionar(@RequestBody Restaurante restaurante){
+
+	@PostMapping // foi substuituido o <Restaurante> por <?> devido conflito pois só estava não
+					// aceitaria a string
+	public ResponseEntity<?> adicionar(@RequestBody Restaurante restaurante) {
 		try {
-		restaurante =  CadastroRestauranteService.Salvar(restaurante);
-		
-		return ResponseEntity.status(HttpStatus.CREATED).body(restaurante);
-		
-		}catch (EntidadeNaoEncontradaException e) {
+			restaurante = CadastroRestauranteService.Salvar(restaurante);
+
+			return ResponseEntity.status(HttpStatus.CREATED).body(restaurante);
+
+		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.badRequest().body(e.getMessage());
 		}
-		
 	}
-	
+
+	@PutMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizar(@PathVariable Long restauranteId, @RequestBody Restaurante restaurante) {
+		try {
+			Restaurante restauranteAtual = restauranteRepository.buscar(restauranteId);
+
+			if (restauranteAtual != null) {
+				BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+
+				restauranteAtual = CadastroRestauranteService.Salvar(restauranteAtual);
+				return ResponseEntity.ok(restauranteAtual);
+			}
+
+			return ResponseEntity.notFound().build();
+
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+
 }
