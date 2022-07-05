5a578ec33d3b8e2ee3695ec1e34b2e18f73c7c63
LeodosSantos <kvn9473@gmail.com>
2022-07-05 18:43:44 -0300

    Criando consultas com query methods

--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -1,13 +1,18 @@
 package com.algaworks.algafood.domain.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Cozinha;
 
-@Component
+@Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
-	//List<Cozinha> consultarPorNome(String nome);
+	List<Cozinha> findTodasByNome(String nome);
+	
+	Optional<Cozinha> findByNome(String nome);
 	
 }
