1e41adec4c75f198c8cd7bbb378f518c3153f244
LeodosSantos <kvn9473@gmail.com>
2022-07-14 19:13:25 -0300

    Testando e analisando o impacto da incorporação de classe na REST API

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,15 @@
 insert into cozinha (id, nome) values (1, 'Tailandesa');
 insert into cozinha (id, nome) values (2, 'Indiana');
 
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Goumert', 10,1);
+insert into estado (id, nome) values (1, 'Sao Paulo');
+insert into estado (id, nome) values (2, 'Rio de Janeiro');
+
+insert into cidade (nome, estado_id) values ('Uba',1);
+insert into cidade (nome, estado_id) values ('Sao Paulo', 1);
+insert into cidade (nome, estado_id) values ('Petropolis', 2);
+insert into cidade (nome, estado_id) values('Rio de Janeiro', 2);
+
+insert into restaurante (nome, taxa_frete, cozinha_id, endereco_cidade_id, endereco_cep, endereco_logradouro, endereco_numero, endereco_complemento,endereco_bairro) values ('Thai Goumert', 10.0, 1, 1, '38400-999', 'Rua João Pinnheiro', '1000', 'centro' );
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Delivery', 9.5 ,1);
 insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida Indiana', 15, 2);
 insert into restaurante (nome, taxa_frete, cozinha_id) values('thaYFood', 5.5, 1);
@@ -15,14 +23,6 @@ insert into forma_pagamento(id, nome) value (4,'pix')
 insert into permissao(id, nome, descricao) value (1,'baixa', 'Apenas consulta')
 insert into permissao(id, nome, descricao) value (2,'media', 'consulta e venda')
 insert into permissao(id, nome, descricao) value (3,'alta', 'consulta venda e cancelamentos')
-
-insert into estado (id, nome) values (1, 'Sao Paulo');
-insert into estado (id, nome) values (2, 'Rio de Janeiro');
-
-insert into cidade (nome, estado_id) values ('Uba',1);
-insert into cidade (nome, estado_id) values ('Sao Paulo', 1);
-insert into cidade (nome, estado_id) values ('Petropolis', 2);
-insert into cidade (nome, estado_id) values('Rio de Janeiro', 2);
 
 insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3),(2, 3), (3, 2),(3,3);
 
